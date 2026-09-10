59faaa524d457142fc16dd220b228fc770e3d814
Paolo Errani <perrani@arpae.it>
2026-09-10 14:24:41 +0200

    PWA: icone dall'immagine di Paolo (testa con frattura nella cornice tonda)

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['favicon.svg', 'favicon.ico', 'apple-touch-icon.png'],
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png'],
       manifest: {
         name: "Diario dell'emicrania",
         short_name: 'Emicrania',
